f816f6ab5ec8dbcec17003f6ac0e6a99ca07c13f
Daulin <daulinri@gmail.com>
2021-08-25 09:05:15 -0500

    se actualiza funciones

--- js/app.js
+++ js/app.js
@@ -1,81 +1,77 @@
 const template = document.querySelector('#grid-image').content;
 const main = document.querySelector('.grid__container');
+
+const all = document.querySelector('#cargarApi');
 const branding = document.querySelectorAll('.menu__links')[1];
 const web = document.querySelectorAll('.menu__links')[2];
 const photo = document.querySelectorAll('.menu__links')[3];
 const app = document.querySelectorAll('.menu__links')[4];
 
+const key = '8PoHaaF1QBWLgByh-uj4owUNFLQAVlyAZ8li4HDgskE';
+    const url =`https://api.unsplash.com/search/collections?page=20&query=app,web,photo,branding&client_id=${key}`;
 
 
 // AddEvenListener
 
-branding.addEventListener('click', (e) => {
-    cleanHTML(); 
-    searchCategory(e);
-});
+all.addEventListener('click', obtenerImagenes);
 
-web.addEventListener('click', (e) => {
-    cleanHTML(); 
-    searchCategory(e);
-});
+branding.addEventListener('click', e => searchCategory(e));
 
-photo.addEventListener('click', (e) => {
-    cleanHTML(); 
-    searchCategory(e);
-});
+web.addEventListener('click', e => searchCategory(e));
 
-app.addEventListener('click', (e) => {
-    cleanHTML(); 
-    searchCategory(e);
-});
+photo.addEventListener('click', e => searchCategory(e));
 
+app.addEventListener('click', (e) => searchCategory(e));
 
+//Funciones
 
+async function obtenerImagenes(e){
+    e.preventDefault();
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        cleanHTML();
+        paintGrid(data);
+    }catch{
+        console.log(error)
+    }
+}
 
-  document.addEventListener('DOMContentLoaded', () =>{
-    fechDate();
-   
- });
-
- const fechAll = async() =>{
-
-    const key = '8PoHaaF1QBWLgByh-uj4owUNFLQAVlyAZ8li4HDgskE';
-    const url =`https://api.unsplash.com/search/collections?page=1&query=web,branding&client_id=${key}`;
-
-      try {
-          const res = await fetch(url);
-          const date = await res.json();
-          console.log(date)
-          console.log(date)
-      } catch (error) {
-           console.log(error)
-      }
-  }
-
-const fechDate = async(category = 'all') =>{
-
-    const key = '8PoHaaF1QBWLgByh-uj4owUNFLQAVlyAZ8li4HDgskE';
-    const url =`https://api.unsplash.com/search/photos/?query=${category}&client_id=${key}`;
-
-      try {
-          const res = await fetch(url);
-          const date = await res.json();
-          console.log(date)
-          paintGrid(date);
-      } catch (error) {
-           console.log(error)
-      }
-  }
-
- const paintGrid = (images) =>{
-     //console.log(images)
-     images.results.forEach(items => {
-       //  console.log(items)
-          const {urls} = items;
+const paintGrid = (dato) =>{   
+     dato.results.forEach(items => {
+        //   console.log(items.cover_photo)
+          const { cover_photo: {urls: {regular}} } = items;
           const clone = template.cloneNode(true);
           const fragment = document.createDocumentFragment();
 
-          clone.querySelector('img').setAttribute('src', urls.regular);
+            clone.querySelector('img').setAttribute('src', regular);
+
+          fragment.appendChild(clone);
+        main.appendChild(fragment);
+    });
+}
+
+
+const fechDate = async(category) =>{
+     const urls =`https://api.unsplash.com/search/photos/?query=${category}&client_id=${key}`;
+
+     try {
+         const res = await fetch(urls);
+         const date = await res.json();
+         categoryGrid(date);
+     } catch (error) {
+          console.log(error)
+     }
+ }
+
+const categoryGrid = (datos) =>{
+     datos.results.forEach(items => {
+          console.log(items.urls)
+          const { urls: {regular} } = items;
+          const clone = template.cloneNode(true);
+          const fragment = document.createDocumentFragment();
+
+           clone.querySelector('img').setAttribute('src', regular);
 
           fragment.appendChild(clone);
         main.appendChild(fragment);
@@ -83,9 +79,10 @@ const fechDate = async(category = 'all') =>{
 }
 
 const searchCategory = (e) =>{
-     const name = e.target.textContent;
-      fechDate(name)
-              
+    e.preventDefault();
+     cleanHTML();
+      const nameCategory = e.target.textContent;
+      fechDate(nameCategory)           
 }
 
 const cleanHTML = () =>{
@@ -93,4 +90,3 @@ const cleanHTML = () =>{
         main.removeChild(main.firstChild)
     }
 }
-
--- js/main.js
+++ js/main.js
@@ -1,39 +1,50 @@
+const alls = document.querySelector('#all');
 const brand = document.querySelectorAll('.grid__menu-item')[1];
 const we = document.querySelectorAll('.grid__menu-item')[2];
 const photography = document.querySelectorAll('.grid__menu-item')[3];
 const ap = document.querySelectorAll('.grid__menu-item')[4];
 
-
 // AddEvenListener
+alls.addEventListener('click', obtImag);
 
 brand.addEventListener('click', (e) => {
-    cleanHTML(); 
     searchCategory(e);
 });
 
 we.addEventListener('click', (e) => {
-    cleanHTML(); 
     searchCategory(e);
 });
 
 photography.addEventListener('click', (e) => {
-    cleanHTML(); 
     searchCategory(e);
 });
 
 ap.addEventListener('click', (e) => {
-    cleanHTML(); 
     searchCategory(e);
 });
 
-const div = document.createElement('div');
+async function obtImag(e){
+    e.preventDefault();
+    const key = '8PoHaaF1QBWLgByh-uj4owUNFLQAVlyAZ8li4HDgskE';
+    const url =`https://api.unsplash.com/search/collections?page=20&query=app,web,photo,branding&client_id=${key}`;
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data)
+        cleanHTML();
+        paintGrid(data);
+    }catch{
+        console.log(error)
+    }
+}
+// const div = document.createElement('div');
 
-div.innerHTML = `
-    <p>CREATIVE LOGO</p>
-    <hr class="line">
-    <h4>Branding</h4>
-`;
+// div.innerHTML = `
+//     <p>CREATIVE LOGO</p>
+//     <hr class="line">
+//     <h4>Branding</h4>
+// `;
 
-div.classList.add('grid-main');
-const inserImage = document.querySelector('.grid__container');
-inserImage.insertBefore(div, inserImage.children[1]);
+// div.classList.add('grid-main');
+// const inserImage = document.querySelector('.grid__container');
+// inserImage.insertBefore(div, inserImage.children[1]);
